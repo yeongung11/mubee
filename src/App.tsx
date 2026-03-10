9fa15df0c074f5777bc00ff97f0c7e2f08242e24
yeongung Kim <sig996@naver.com>
2026-03-10 15:58:47 +0900

    Router로 app.tsx에서 Home으로 기능 및 페이지 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import { useEffect, useState } from "react";
-import { fetchPopularMovies, fetchUpcomingMovies } from "./api/tmdb";
-import type { Movie } from "./types/movie";
-import { MovieRanking } from "./components/MovieRanking";
-import { Header } from "./components/Header";
-import { Upcoming } from "./components/Upcoming";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+// import { Detail } from "./pages/Detail";
 
 function App() {
-    const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
-    const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const loadAllData = async () => {
-            setLoading(true);
-            try {
-                const [popularRes, upcomingRes] = await Promise.all([
-                    fetchPopularMovies(),
-                    fetchUpcomingMovies(),
-                ]);
-                setPopularMovies(popularRes.results);
-                setUpcomingMovies(upcomingRes.results);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
-        };
-        loadAllData();
-    }, []);
-
-    if (loading) return <div> 로딩중...</div>;
-
     return (
-        <>
-            <Header />
-            <MovieRanking movies={popularMovies} />
-            <Upcoming movies={upcomingMovies}/>
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/movie/:id" element={<Detail />} /> */}
+            </Routes>
+        </BrowserRouter>
     );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { fetchPopularMovies, fetchUpcomingMovies } from "../api/tmdb";
+import type { Movie } from "../types/movie";
+import { MovieRanking } from "../components/MovieRanking";
+import { Header } from "../components/Header";
+import { Upcoming } from "../components/Upcoming";
+
+export function Home() {
+    const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+    const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const loadAllData = async () => {
+            setLoading(true);
+            try {
+                const [popularRes, upcomingRes] = await Promise.all([
+                    fetchPopularMovies(),
+                    fetchUpcomingMovies(),
+                ]);
+                setPopularMovies(popularRes.results);
+                setUpcomingMovies(upcomingRes.results);
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        loadAllData();
+    }, []);
+
+    if (loading) return <div> 로딩중...</div>;
+
+    return (
+        <>
+            <Header />
+            <MovieRanking movies={popularMovies} />
+            <Upcoming movies={upcomingMovies} />
+        </>
+    );
+}
